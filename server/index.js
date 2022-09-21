91e25494510d60792d9da9161255b1b4a784f024
Propulsive60r <Propulsive60r@yandex.ru>
2022-09-21 17:11:42 +0300

    3 commit

--- server/index.js
+++ server/index.js
@@ -1,1 +1,7 @@
+require('dotenv').config() //подключаем для работы с окружением
+
 const express = require('express')
+const PORT = process.env.PORT || 5000 //забираем переменную с окружения .env
+const app = express()
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
