6b6f118438377dc37708ff30dbb125063cd29e36
Propulsive60r <Propulsive60r@yandex.ru>
2022-09-30 10:06:05 +0300

    5 add Sequelize export

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 require('dotenv').config() //подключаем для работы с окружением
 
 const express = require('express')
+const sequelize = require('./db')
 const PORT = process.env.PORT || 5000 //забираем переменную с окружения .env
 const app = express()
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
+
